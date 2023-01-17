410581f6f3174a5a8dd95f4b1aa4aa9360896caa
Denis Diedov <80519500+Wan-de-Rock@users.noreply.github.com>
2023-01-17 19:04:36 +0100

    project init

---
+++ project/EventRegestranionSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventRegestranionSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
